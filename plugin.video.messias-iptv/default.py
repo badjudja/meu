accc0df146139b68993c310f5469fc6afa7ca369
Messias <badjudja@gmail.com>
2016-07-13 23:57:25 +0100

    update

--- plugin.video.messias-iptv/default.py
+++ plugin.video.messias-iptv/default.py
@@ -36,10 +36,12 @@ REMOTE_VERSION_FILE = "http://dofundo.mooo.com/version.xml"
 
 online_m3u = 'http://dofundo.mooo.com/Ptlista.m3u'
 local_m3u = 'http://dofundo.mooo.com/Ptlista2.m3u'
+pt_m3u = 'http://dofundo.mooo.com/Ptlista2.m3u'
 online_xml = 'http://dofundo.mooo.com/lista.m3u'
 local_xml = 'http://dofundo.mooo.com/lista2.m3u'
 algas_m3u = 'http://pastebin.com/raw/6UwwrXRN'
 brasil_m3u = 'http://pastebin.com/raw/HyFjh35W'
+mega_xml = 'http://pastebin.com/raw/vHQ5EBLq'
 
 xml_regex = '<title>(.*?)</title>\s*<link>(.*?)</link>\s*<thumbnail>(.*?)</thumbnail>'
 m3u_thumb_regex = 'tvg-logo=[\'"](.*?)[\'"]'
@@ -119,7 +121,6 @@ def find_single_match(data,patron,index=0):
     except:
         return ""
     
-#url = 'http://vaniarupeni.altervista.org/repo/plugin.video.kodilivetv-1.1.0.zip'
 percent = 0
 def DownloaderClass(url,dest):
     dp = xbmcgui.DialogProgress()
@@ -139,28 +140,38 @@ def read_file(file):
     except:
         pass
 
-def make_request(url):
-	try:
-		req = urllib2.Request(url)
-		req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0')
-		response = urllib2.urlopen(req)	  
-		link = response.read()
-		response.close()  
-		return link
-	except urllib2.URLError, e:
-		print 'We failed to open "%s".' % url
-		if hasattr(e, 'code'):
-			print 'We failed with error code - %s.' % e.code	
-		if hasattr(e, 'reason'):
-			print 'We failed to reach a server.'
-			print 'Reason: ', e.reason
+def addon_log(string):
+    if debug == 'true':
+        xbmc.log("[addon.messias-iptv-%s]: %s" %(addon_version, string))
+
+def make_request(url, headers=None):
+        try:
+            if headers is None:
+                headers = {'User-agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0'}
+            req = urllib2.Request(url,None,headers)
+            response = urllib2.urlopen(req)
+            data = response.read()
+            response.close()
+            return data
+        except urllib2.URLError, e:
+            addon_log('URL: '+url)
+            if hasattr(e, 'code'):
+                addon_log('We failed with error code - %s.' % e.code)
+                xbmc.executebuiltin("XBMC.Notification(messias-iptv,We failed with error code - "+str(e.code)+",10000,"+icon+")")
+            elif hasattr(e, 'reason'):
+                addon_log('We failed to reach a server.')
+                addon_log('Reason: %s' %e.reason)
+                xbmc.executebuiltin("XBMC.Notification(messias-iptv,We failed to reach a server. - "+str(e.reason)+",10000,"+icon+")")			
+			
 			
 def main():
 	add_dir('[B]<<<  Pesquisar  >>>[/B]', 'searchlink', 99, icon, fanart)
 	if len(online_m3u) > 0:	
-		add_dir('[COLOR yellow][B]>> Nacional 1 <<[/B][/COLOR]', u_tube, 2, icon, fanart)
+		add_dir('[COLOR magenta][B]>> Nacional 1 <<[/B][/COLOR]', u_tube, 2, icon, fanart)
 	if len(local_m3u) > 0:	
 		add_dir('[COLOR magenta][B]>> Nacional 2 <<[/B][/COLOR]', u_tube, 3, icon, fanart)
+	if len(pt_m3u) > 0:	
+		add_dir('[COLOR magenta][B]>> Nacional 3 <<[/B][/COLOR]', u_tube, 9, icon, fanart)
 	if len(online_xml) > 0:	
 		add_dir('[COLOR cyan][B]>> Internacional 1<<[/B][/COLOR]', u_tube, 4, icon, fanart)
 	if len(local_xml) > 0:	
@@ -169,7 +180,9 @@ def main():
 		add_dir('[COLOR blue][B]>> TV Algas <<[/B][/COLOR]', u_tube, 6, icon, fanart)	
 	if len(brasil_m3u) > 0:	
 		add_dir('[COLOR green][B]>> Lista Brasil <<[/B][/COLOR]', u_tube, 7, icon, fanart)	
-	if (len(online_m3u) < 1 and len(local_m3u) < 1 and len(online_xml) < 1 and len(local_xml) < 1 and len(algas_m3u) < 1 and len(brasil_m3u) < 1 ):		
+	if len(mega_xml) > 0:	
+		add_dir('[COLOR red][B]>> TV Mega <<[/B][/COLOR]', u_tube, 8, icon, fanart)	
+	if (len(online_m3u) < 1 and len(local_m3u) < 1 and len(online_xml) < 1 and len(local_xml) < 1 and len(algas_m3u) < 1 and len(brasil_m3u) < 1 and len(mega_xml) < 1 and len(pt_m3u) < 1 ):		
 		mysettings.openSettings()
 		xbmc.executebuiltin("Container.Refresh")		
 
@@ -214,7 +227,19 @@ def search():
 			match = re.compile(m3u_regex).findall(content)
 			for thumb, name, url in match:
 				if re.search(searchText, removeAccents(name.replace('Đ', 'D')), re.IGNORECASE):
-					m3u_playlist(name, url, thumb)				
+					m3u_playlist(name, url, thumb)		
+		if len(mega_xml) > 0:					
+			content = make_request(mega_xml)
+			match = re.compile(xml_regex).findall(content)	
+			for name, url, thumb in match:
+				if re.search(searchText, removeAccents(name.replace('Đ', 'D')), re.IGNORECASE):
+					xml_playlist(name, url, thumb)	
+		if len(pt_m3u) > 0:		
+			content = make_request(pt_m3u)
+			match = re.compile(m3u_regex).findall(content)
+			for thumb, name, url in match:
+				if re.search(searchText, removeAccents(name.replace('Đ', 'D')), re.IGNORECASE):
+					m3u_playlist(name, url, thumb)						
 	except:
 		pass
 		
@@ -268,7 +293,23 @@ def m3u_brasil():
 		try:
 			m3u_playlist(name, url, thumb)
 		except:
-			pass				
+			pass		
+def xml_mega():			
+	content = make_request(mega_xml)
+	match = re.compile(m3u_regex).findall(content)
+	for thumb, name, url in match:
+		try:
+			m3u_playlist(name, url, thumb)
+		except:
+			pass	
+def m3u_pt():		
+	content = make_request(pt_m3u)
+	match = re.compile(m3u_regex).findall(content)
+	for thumb, name, url in match:	
+		try:
+			m3u_playlist(name, url, thumb)
+		except:
+			pass			
 def m3u_playlist(name, url, thumb):	
 	name = re.sub('\s+', ' ', name).strip()			
 	url = url.replace('"', ' ').replace('&amp;', '&').strip()
@@ -319,6 +360,7 @@ def play_video(url):
 	xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, item)
 	return
 
+
 def get_params():
 	param = []
 	paramstring = sys.argv[2]
@@ -356,7 +398,8 @@ def add_link(name, url, mode, iconimage, fanart):
 	liz.setProperty('fanart_image', fanart)
 	liz.setProperty('IsPlayable', 'true') 
 	ok = xbmcplugin.addDirectoryItem(handle = int(sys.argv[1]), url = u, listitem = liz)  
-		
+	
+	
 params = get_params()
 url = None
 name = None
@@ -391,6 +434,7 @@ if mode == None or url == None or len(url) < 1:
 elif mode == 1:
 	play_video(url)
 
+
 elif mode == 2:
 	m3u_online()
 	
@@ -408,7 +452,13 @@ elif mode == 6:
 	
 elif mode == 7:
 	m3u_brasil()
-
+	
+elif mode == 8:
+	xml_mega()
+	
+elif mode == 9:
+	m3u_pt()
+	
 elif mode == 99:
 	search()
 	
